3aaceb3e8f7ae984bea501db70a530f357837348
TooOPLive <toooplive@gmail.com>
2019-05-21 18:20:22 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>TooOPLive</title>
+  <meta name="description" content="TooOPWorld">
+  <meta name="author" content="TooOPLive">
+
+  <link rel="stylesheet" href="./css/styles.css">
+
+</head>
+
+<body class="background">
+
+	<div class="container">
+		<div class="header">
+			<header>
+				<div class="headnav">
+					<a href="#"><img class="logo" src="./images/logo-min.png"></a>
+					
+					<ul class="navbar">
+						<li><a href="#">About</a></li>
+						<li><a href="#">Portfolio</a></li>
+						<li><a href="#">Pricing</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+					<ul class="navbar twitch">
+						<li><a href="#"><img class="glitch" src="./images/glitch-white-min.png"></a></li>
+					</ul>
+				
+				</div>
+			</header>
+			
+			<section>
+				<div class="content">
+					<h1 class="headline">I Create Designs for Streamers</h1>
+					<p class="subtopic">Level up your stream branding with personalised overlays,<br> panels, banners, alerts and even your very own website!</p>
+				</div>
+			</section>
+		</div>
+		
+		<div class="footer">
+			<footer>
+				<div class="footnav">
+					<div class="copyright">
+						&copy; TooOPLive 2019
+					</div>
+					<ul class="navbar mediabuttons">
+						<li><a href="#">Twitch</a></li>
+						<li><a href="#">Facebook</a></li>
+						<li><a href="#">Twitter</a></li>
+						<li><a href="#">YouTube</a></li>
+						<li><a href="#">Instagram</a></li>
+					</ul>
+				</div>
+			</footer>
+		</div>
+		
+	</div>
+	
+	<!-- <script src="js/scripts.js"></script> -->
+</body>
+</html>
